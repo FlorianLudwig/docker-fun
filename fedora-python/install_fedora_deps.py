33a3acdaea74a36a948f1dfe33fe358e6f840c50
Florian Ludwig <f.ludwig@greyrook.com>
2016-08-11 14:51:37 +0200

    more modules

--- fedora-python/install_fedora_deps.py
+++ fedora-python/install_fedora_deps.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python
 import sys
 import subprocess
-# install system PACKAGES for python packages
 
+# install system PACKAGES for python packages
 build_system = ['redhat-rpm-config', 'gcc']
+build_system_ubuntu = ['build-essential']
+
 
 PACKAGES = {
     'pillow': {
         'fedora': {
             'build': build_system + ['lcms2-devel', 'zlib-devel', 'libjpeg-turbo-devel', 'freetype-devel', 'openjpeg2-devel', 'libtiff-devel', 'libwebp-devel'],
             'run': ['lcms2', 'zlib', 'libjpeg-turbo', 'freetype', 'openjpeg2', 'libtiff', 'libwebp']
+        },
+        'ubuntu': {
+            'build': build_system_ubuntu + ['liblcms2-dev', 'liblz-dev', 'libjpeg-turbo8-dev', 'libfreetype6-dev', 'libopenjpeg-dev', 'libtiff5-dev', 'libwebp-dev'],
+            'run': ['liblcms2-2']
         }
     },
     'gitpython': {
         'fedora': {
             'run': ['git']
+        }
+    },
+    'av': {
+        'fedora': {
+            'run': ['git'],
+            'build': ['ffmpeg-devel']
+        }
+    },
+    'cryptography': {
+        'fedora': {
+            'build': ['libffi-devel', 'openssl-devel']
+        }
+    },
+    'lxml': {
+        'fedora': {
+            'build': ['libxml2-devel', 'libxslt-devel']
         }
     }
 }
@@ -36,10 +58,11 @@ def main():
                 run_packages.update(deps.get('run', []))
 
     packages = build_packages.union(run_packages)
-    cmd = ['dnf', 'install', '-y']
-    cmd += packages
-    print cmd
-    subprocess.check_call(cmd)
+    if packages:
+        cmd = ['dnf', 'install', '-y']
+        cmd += packages
+        print cmd
+        subprocess.check_call(cmd)
 
 
 if __name__ == '__main__':
